73ae2639974103c1672696fad5c73b8d72809408
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-05-24 16:18:16 -0400

    Update providers.tf

--- cloud/azure/templates/azure_saml_ses/providers.tf
+++ cloud/azure/templates/azure_saml_ses/providers.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
 
 provider "aws" {
   region = var.aws_region
-  access_key = var.aws-access-key-id
-  secret_key = var.aws-secret-access-token
+  access_key = locals.aws-access-key-id
+  secret_key = locals.aws-secret-access-token
 }
