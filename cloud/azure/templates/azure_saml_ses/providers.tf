2aa84f1664499869bc1a95758e6e61d97074e88a
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-05-24 16:49:34 -0400

    Revert providers.tf to original

--- cloud/azure/templates/azure_saml_ses/providers.tf
+++ cloud/azure/templates/azure_saml_ses/providers.tf
@@ -4,6 +4,4 @@ provider "azurerm" {
 
 provider "aws" {
   region = var.aws_region
-  access_key = data.azurerm_key_vault_secret.aws_access_key_id.value
-  secret_key = data.azurerm_key_vault_secret.aws_secret_access_token.value
 }
