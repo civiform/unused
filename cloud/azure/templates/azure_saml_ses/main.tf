1399030d20741510616587dbce74bc59102a60a5
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-05-24 15:31:51 -0400

    Update main.tf

--- cloud/azure/templates/azure_saml_ses/main.tf
+++ cloud/azure/templates/azure_saml_ses/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-    # removing AWS provider
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "5.15.0"
-    # }
+     removing AWS provider
+     aws = {
+       source  = "hashicorp/aws"
+       version = "5.15.0"
+    }
     azurerm = {
       source  = "azurerm"
       version = "3.70.0"
