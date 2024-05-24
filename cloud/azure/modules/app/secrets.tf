cabca1660cdc4ce9721c3c94d7103ad4c527df2a
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-05-24 16:30:02 -0400

    add data for aws secrets

--- cloud/azure/modules/app/secrets.tf
+++ cloud/azure/modules/app/secrets.tf
@@ -32,3 +32,13 @@ data "azurerm_key_vault_secret" "adfs_secret" {
   name         = local.adfs_secret_keyvault_id
   key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
 }
+
+data "azurerm_key_vault_secret" "aws_access_key_id" {
+  name         = local.aws_access_key_id
+  key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
+}
+
+data "azurerm_key_vault_secret" "aws_secret_access_token" {
+  name         = local.aws_secret_access_token
+  key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
+}
