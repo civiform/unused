5f778b7d0e976ed0bb364bb6bd68e5b9bef208b4
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-05-24 16:41:10 -0400

    Delete cloud/azure/templates/azure_saml_ses/secrets.tf

--- cloud/azure/templates/azure_saml_ses/secrets.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_key_vault_secret" "aws_secret_access_token" {
-  name         = local.aws_secret_access_token
-  key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
-}
-
-data "azurerm_key_vault_secret" "aws_access_key_id" {
-  name         = local.aws_access_key_id
-  key_vault_id = data.azurerm_key_vault.civiform_key_vault.id
-}
