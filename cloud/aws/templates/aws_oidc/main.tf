23a2b41f2cb51276a3fd44f08c56ff9dbc519b52
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-06-28 13:33:00 -0400

    Update sg reference

--- cloud/aws/templates/aws_oidc/main.tf
+++ cloud/aws/templates/aws_oidc/main.tf
@@ -146,7 +146,7 @@ resource "aws_security_group" "rds" {
       from_port       = 5432
       to_port         = 5432
       protocol        = "tcp"
-      security_groups = [module.ecs_fargate_service.aws_security_group_lb_access_sg_id]
+      security_groups = [module.ecs_fargate_service.aws_security_group_ecs_tasks_access_sg_id]
     }
   }
 }
