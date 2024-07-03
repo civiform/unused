2c7896bf7a1465ff948b7d7cb3252631effd1e22
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-07-03 15:31:53 -0400

    Update write_tfvars.py

--- cloud/shared/bin/lib/write_tfvars.py
+++ cloud/shared/bin/lib/write_tfvars.py
@@ -56,12 +56,17 @@ class TfVarWriter:
                     print(definition)
 
                     if definition is not None:
+                        is_list = False
                         try:
-                            json_def = json.loads(definition)
-                            if isinstance(json_def, list):
-                                print("LIST TYPE")
-                                tf_vars_file.write(f'{name.lower()}={definition}\n')
-                            else:
-                                tf_vars_file.write(f'{name.lower()}="{definition}"\n')
-                        except json.JSONDecodeError as e:
-                            print(f"Invalid JSON for '{name}': {e}")
+                            is_list = isinstance(json.loads(definition), list)
+                        #     if isinstance(json_def, list):
+                        #         print("LIST TYPE")
+                        #         tf_vars_file.write(f'{name.lower()}={definition}\n')
+                        #     else:
+                        #         tf_vars_file.write(f'{name.lower()}="{definition}"\n')
+                        # except json.JSONDecodeError as e:
+                        #     print(f"Invalid JSON for '{name}': {e}")
+                        if is_list:
+                            tf_vars_file.write(f'{name.lower()}={definition}\n')
+                        else:
+                            tf_vars_file.write(f'{name.lower()}="{definition}"\n')
