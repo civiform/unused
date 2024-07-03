f864a16cfbd5ef6397262e5305df84ab30d798a8
dkatzz <86739416+dkatzz@users.noreply.github.com>
2024-07-03 11:56:09 -0400

    try list another way

--- cloud/shared/bin/lib/write_tfvars.py
+++ cloud/shared/bin/lib/write_tfvars.py
@@ -24,23 +24,27 @@ class TfVarWriter:
                         f"{Variables.CIVIFORM_SERVER_VARIABLES_KEY} = {{\n")
                     for key, value in definition.items():
                         if value is not None:
-                            tf_vars_file.write(f'  "{key}"="{value}"\n')
+                            if isinstance(value, list):
+                                print("LIST TYPE")
+                                tf_vars_file.write(f'{key.lower()}={value}\n')
+                            else:
+                                tf_vars_file.write(f'  "{key}"="{value}"\n')
                     tf_vars_file.write("}\n")
                     continue
 
-                # Special handling for "list" type variables
-                if name == Variables.TERRAFORM_LIST_VARIABLES_KEY:
-                    print("THIS IS THE DEF")
-                    print(definition)
-                    print("DEF ITEMS")
-                    print(definition.items())
-                    for key, value in definition.items():
-                        print("THIS IS THE KEY")
-                        print(key)
-                        print("THIS IS THE VALUE")
-                        print(value)
-                        if value is not None:
-                            tf_vars_file.write(f'{key.lower()}={value}\n')
+                # # Special handling for "list" type variables
+                # if name == Variables.TERRAFORM_LIST_VARIABLES_KEY:
+                #     print("THIS IS THE DEF")
+                #     print(definition)
+                #     print("DEF ITEMS")
+                #     print(definition.items())
+                #     for key, value in definition.items():
+                #         print("THIS IS THE KEY")
+                #         print(key)
+                #         print("THIS IS THE VALUE")
+                #         print(value)
+                #         if value is not None:
+                #             tf_vars_file.write(f'{key.lower()}={value}\n')
 
                 elif definition is not None:
                     tf_vars_file.write(f'{name.lower()}="{definition}"\n')
